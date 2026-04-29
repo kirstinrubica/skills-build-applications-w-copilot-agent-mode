c811d428a8f1ae1e64fcbaeccdd746825d18202c
Kirstin Rubica <78091015+kirstinrubica@users.noreply.github.com>
2026-04-29 09:33:29 +0000

    Fix MongoDB auth for local dev, ensure codespace API URLs

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-2u-t=sl=gw(cl4i=#c4n42g62a1g2r!m*c5+c5p2+cb4y9z67$
 DEBUG = True
 
 import os
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if os.environ.get('CODESPACE_NAME'):
+    ALLOWED_HOSTS.append(f"{os.environ.get('CODESPACE_NAME')}-8000.app.github.dev")
 
 
 # Application definition
@@ -86,10 +88,6 @@ DATABASES = {
         'ENFORCE_SCHEMA': False,
         'CLIENT': {
             'host': 'mongodb://localhost:27017',
-            'username': '',
-            'password': '',
-            'authSource': 'admin',
-            'authMechanism': 'SCRAM-SHA-1',
         },
     }
 }
